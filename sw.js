64b810d34dfe887377b6eb6ae25830b9f97e8b5e
Ennio <ehesseboom@hotmail.com>
2025-06-29 14:56:55 +0200

    updated pwa

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const cacheAssets = [
   "/index.html",
   "/manifest.json",
   "/script.js",
-  "/style.css", // or whatever your main CSS file is
+  "/style.css",
   "/assets/images/app-icon-192.png",
   "/assets/images/app-icon-512.png",
 ];
@@ -13,14 +13,51 @@ const cacheAssets = [
 self.addEventListener("install", (e) => {
   console.log("Service Worker: Installing...");
   e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      console.log("Service Worker: Caching files");
-      return cache.addAll(cacheAssets);
-    })
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        console.log("Service Worker: Caching files");
+        return cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate event
+self.addEventListener("activate", (e) => {
+  console.log("Service Worker: Activating...");
+  e.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((name) => {
+            if (name !== cacheName) {
+              return caches.delete(name);
+            }
+          })
+        );
+      })
+      .then(() => self.clients.claim())
   );
 });
 
 // Fetch event
 self.addEventListener("fetch", (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    caches
+      .match(e.request)
+      .then((cachedResponse) => {
+        const networkFetch = fetch(e.request).then((networkResponse) => {
+          if (e.request.url.includes("f1api.dev")) {
+            caches.open(cacheName).then((cache) => {
+              cache.put(e.request, networkResponse.clone());
+            });
+          }
+          return networkResponse;
+        });
+        return cachedResponse || networkFetch;
+      })
+      .catch(() => caches.match("/index.html"))
+  );
 });
