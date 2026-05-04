0c3c7aa976b5e2330af3312e5244f62ae538f122
Ennio <ehesseboom@hotmail.com>
2026-05-04 11:17:36 +0200

    fixed data not showing

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
-const CACHE_NAME = "my-app-cache-v1";
+const CACHE_NAME = "my-app-cache-v2";
 const urlsToCache = [
   "index.html",
   "style.css",
-  "script.js",
   "assets/images/web-app-manifest-192x192.png",
   "assets/images/web-app-manifest-512x512.png",
   "assets/images/screenshot-desktop.png",
@@ -13,7 +12,7 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
-    })
+    }),
   );
 });
 
@@ -21,6 +20,6 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
-    })
+    }),
   );
 });
